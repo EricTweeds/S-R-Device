fbbe3c5ac27378d6e7f9eed5c163518c56969c6e
bradleymcallister97 <brad.mcallister@hotmail.com>
2019-03-08 19:00:59 -0500

    add map manager lib

---
+++ libraries/MapManagerLib/MapManagerLib.cpp
@@ -0,0 +1,37 @@
+#include "Arduino.h"
+#include "MapManagerLib.h"
+
+MapManagerLib::MapManagerLib() {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++){
+            MAP[i][j] = UNKNOWN;
+        }
+    }
+}
+
+void MapManagerLib::setMapValue(int x, int y, char value) {
+    // assuming value is an acceptable char value
+    // assuming that x and y are less than 6
+    MAP[x][y] = value;
+}
+
+char MapManagerLib::getMapValue(int x, int y) {
+    // assuming that x and y values are less than size
+    return MAP[x][y];
+}
+
+bool MapManagerLib::getLocation(int &x, int &y, char value) {
+    // Returns true if value is found
+    // x and y are assigned the coordinates of the value
+    // This function would be used to find the goals (e.g person or candle)
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++){
+            if (MAP[i][j] == value) {
+                x = i;
+                j = y;
+                return true;
+            }
+        }
+    }
+    return false;
+}
---
+++ libraries/MapManagerLib/MapManagerLib.h
@@ -0,0 +1,31 @@
+#ifndef MapManagerLib_h
+#define MapManagerLib_h
+
+#include "Arduino.h"
+
+class MapManagerLib
+{
+    private:
+        char MAP[6][6];
+        const int size = 6;
+
+    public:
+        MapManagerLib();
+        
+        // Const values
+        const char UNKNOWN = '?';
+        const char WATER = 'W';
+        const char GROUND = 'G';
+        const char PERSON = 'P';
+        const char GROUP = 'GP';
+        const char FINISH = 'F';
+        const char SAND = 'S';
+        const char ROCK = 'R';
+        const char CANDLE = 'C';
+
+        void setMapValue(int x, int y, char value);
+        char getMapValue(int x, int y);
+        bool getLocation(int &x, int &y, char value);
+};
+
+#endif
