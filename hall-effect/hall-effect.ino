f3a9d70437324fcf0b175df8e9c0c4055b848405
Eric Tweedle <erictweeds@gmail.com>
2019-03-12 23:06:04 -0400

    minor changes to hall-effect code

--- hall-effect/hall-effect.ino
+++ hall-effect/hall-effect.ino
@@ -1,13 +1,8 @@
-volatile byte half_revolutions;
-unsigned int rpm;
-unsigned long timeold;
+
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
     attachInterrupt(0, magnet_detect, RISING);
-    half_revolutions = 0;
-    rpm = 0;
-    timeold = 0;
 }
 
 void loop() {
@@ -15,6 +10,5 @@ void loop() {
 }
 
 void magnet_detect() {
-    half_revolutions++;
     Serial.println("detect");
 }
