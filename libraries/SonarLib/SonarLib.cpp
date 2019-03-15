3ad0800989ef4e5b6296c47eff0f3f88f5de78d5
bradleymcallister97 <brad.mcallister@hotmail.com>
2019-03-14 20:40:31 -0400

    add get average sonar function

--- libraries/SonarLib/SonarLib.cpp
+++ libraries/SonarLib/SonarLib.cpp
@@ -9,23 +9,31 @@ SonarLib::SonarLib(int trig, int echo) {
     pinMode(echoPin, INPUT);
 }
 
-void SonarLib::getDistance() {
+float SonarLib::getDistance() {
     /*
-   * returns distance from given sonar to object in cm.
-   * Called when distance for sonar is required.
-   */
-  
-  // Clear the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  long duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  float distance = duration * 0.034 / 2;
+    * returns distance from given sonar to object in cm.
+    * Called when distance for sonar is required.
+    */
+    
+    // Clear the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    long duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    float distance = duration * 0.034 / 2;
 
-  return distance;
+    return distance;
+}
+
+float getAverageDistance(int numSamples) {
+    float sum = 0;
+    for (int i = 0; i < numSamples; i++) {
+        sum += getDistance();
+    }
+    return sum / numSamples;
 }
