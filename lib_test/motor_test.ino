fd941249c2f36842d0b4bec78620a3890e32e0aa
bradleymcallister97 <brad.mcallister@hotmail.com>
2019-03-16 11:39:08 -0400

    add file to test motor lib

---
+++ lib_test/motor_test.ino
@@ -0,0 +1,20 @@
+#include "MotorLib.h"
+
+#define ENA 10
+#define ENB 9
+#define Right1 5
+#define Right2 4
+#define Left1 6
+#define Left2 7
+
+MotorLib
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    
+}
