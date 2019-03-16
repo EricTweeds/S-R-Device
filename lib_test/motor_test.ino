de98d9638af96c8445635c39a719178cc0ba4c25
bradleymcallister97 <brad.mcallister@hotmail.com>
2019-03-16 11:40:12 -0400

    add motor test code

--- lib_test/motor_test.ino
+++ lib_test/motor_test.ino
@@ -7,11 +7,24 @@
 #define Left1 6
 #define Left2 7
 
-MotorLib
+Motor leftMotor = {ENA, Left1, Left2};
+
+Motor rightMotor = {ENB, Right1, Right2};
+
+MotorLib motors = MotorLib(rightMotor, leftMotor);
 
 void setup()
 {
     Serial.begin(9600);
+    motors.driveForward();
+    delay(3000);
+    motors.driveBackwards();
+    delay(3000);
+    motors.turnLeft();
+    delay(3000);
+    motors.turnRight();
+    delay(3000);
+    motors.updateSpeeds(0, 0);
 }
 
 void loop()
