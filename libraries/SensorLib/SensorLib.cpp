de908cf3abfca6fd110c2a9ac3939bf228297956
Eric Tweedle <eric.tweedle@uwaterloo.ca>
2019-03-20 23:26:05 -0400

    new LED functions and lib cleanup

--- libraries/SensorLib/SensorLib.cpp
+++ libraries/SensorLib/SensorLib.cpp
@@ -1,73 +1,8 @@
 #include "Arduino.h"
 #include "SensorLib.h"
 
-#define S0 3
-#define S1 4
-#define S2 5
-#define S3 6
-#define CSLights 7
-#define sensorOut 2
-
-struct RGB {
-  int R;
-  int G;
-  int B;
-};
-
 SensorLib::SensorLib() {
     //constructor
-}
-
-float SensorLib::getAmbientT(int sensorPin) {
-  /*
-   * returns ambient Temperature of the surroundings.
-   * To be called during start up of device.
-   * Value needed for candleDetection.
-   */
-  const int WaitTime = 500; //milliseconds between each temperature scan.
-  const int samples = 5; //Number of samples used in average
-  
-  int sensorSum = analogRead(sensorPin);
-
-  for (int i = 1; i < samples; i++) {
-    delay(WaitTime);
-    sensorSum += analogRead(sensorPin);
-  }
-
-  float sensorVal = sensorSum/(samples*1.0);
-  float voltage = (sensorVal/1024.0) * 5.0;
-
-  float temperature = (voltage - 0.5) * 100;
-
-  return temperature;
-}
-
-bool SensorLib::candleDetected(int sensorPin, float ambientT) {
-  /*
-   * Requires an input of the ambiet temperature of the surrounding
-   * to be taken during start up.
-   * Takes a specified amount of temperature readings spaced out by
-   * a wait time. Then averages the values to get a more accurate value.
-   * Boolean returned whether difference is greater than threshold.
-   */
-  const int WaitTime = 500; //milliseconds between each temperature scan.
-  const int samples = 3; //Number of samples used in average
-  const float threshold = 2; //temperature change that signifies candle is present
-
-  int sensorSum = analogRead(sensorPin);
-
-  for (int i = 1; i < samples; i++) {
-    delay(WaitTime);
-    sensorSum += analogRead(sensorPin);
-  }
-
-  float sensorVal = sensorSum/(samples*1.0);
-  float voltage = (sensorVal/1024.0) * 5.0;
-
-  float temperature = (voltage - 0.5) * 100;
-
-  //Serial.println(temperature);
-  return (temperature - ambientT) > threshold;
 }
 
 void SensorLib::toggleLED(int pin) {
@@ -118,4 +53,50 @@ void SensorLib::setCSLights(bool on) {
   } else {
     digitalWrite(CSLights, LOW);
   }
+}
+
+void SensorLib::setRGBColour(int colour) {
+  if(colour == 0) { //off
+    _setColour(true, true, true);
+  } else if (colour == 1) { //red
+    _setColour(true, false, false);
+  } else if (colour == 2) { //green
+    _setColour(false, true, false);
+  } else if (colour == 3) { //blue
+    _setColour(false, false, true);
+  } else if (colour == 4) { //yellow
+    _setColour(true, true, false);
+  } else if (colour == 5) { //purple
+    _setColour(true, false, true);
+  } else if (colour == 6) { //aqua
+    _setColour(false, true, true);
+  }
+}
+
+void SensorLib::_setColour(bool r, bool g, bool b) {
+  int redPin = 7;
+  int greenPin = 5;
+  int bluePin = 6;
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  if (r) {
+    digitalWrite(redPin, LOW);
+  } else {
+    digitalWrite(redPin, HIGH);
+  }
+
+  if (g) {
+    digitalWrite(greenPin, LOW);
+  } else {
+    digitalWrite(greenPin, HIGH);
+  }
+  
+  if (b) {
+    digitalWrite(bluePin, LOW);
+  } else {
+    digitalWrite(bluePin, HIGH);
+  } 
 }
--- libraries/SensorLib/SensorLib.h
+++ libraries/SensorLib/SensorLib.h
@@ -1,6 +1,21 @@
 #ifndef SensorLib_h
 #define SensorLib_h
 
+#define S0 3
+#define S1 4
+#define S2 5
+#define S3 6
+#define CSLights 7
+#define sensorOut 2
+
+#define OFF 0
+#define RED 1
+#define GREEN 2
+#define BLUE 3
+#define YELLOW 4
+#define PURPLE 5
+#define AQUA 6
+
 #include "Arduino.h"
 
 struct RGB {
@@ -13,12 +28,12 @@ class SensorLib
 {
   public:
     SensorLib();
-    float getAmbientT(int sensorPin);
-    bool candleDetected(int sensorPin, float ambientT);
     void toggleLED(int pin);
     void logColourVal(RGB colour);
     RGB getColour();
     void setCSLights(bool on);
+    void setRGBColour(int colour);
+    void _setColour(bool r, bool g, bool b);
 };
 
 #endif
